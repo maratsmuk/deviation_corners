201ef040d6f59a9eb70720ca0dbff2cd68752aa0
Marat <72742325+maratsmuk@users.noreply.github.com>
2021-11-05 17:32:51 +0100

    version 0.1

--- myplotlib.py
+++ myplotlib.py
@@ -25,8 +25,25 @@ class plotlib:
         plt.title('Correlation Matrix', fontsize=16);
         #plt.show()
         plt.savefig('plots/[corr_matrix]'+fname+'.png')
-        f2 = pd.plotting.scatter_matrix(df, alpha=0.2)
+        plt.close(f)
+        f2 = plt.figure(figsize=(19, 15))
+        pd.plotting.scatter_matrix(df, alpha=0.2)
         plt.savefig('plots/[scatter_matrix]'+fname+'.png')
+        plt.close(f2)
+        cols = df.select_dtypes(['float']).columns
+        #print(df[[cols[0]]])
+        for i_col in range(0,len(cols)-1):
+            for j_col in range(i_col+1,len(cols)):
+                f3 = plt.figure(figsize=(19, 15))
+                df.plot(kind= 'scatter',x = cols[i_col],y=cols[j_col])
+                plt.savefig('plots/[scatter_'+cols[i_col]+'_vs_'+cols[j_col]+']'+fname+'.png')
+                plt.close(f3)
+            for y_cat_col in df.select_dtypes(['int']).columns:
+                f4 = plt.figure(figsize=(19, 15))
+                df.plot(kind='scatter', x=cols[i_col], y=y_cat_col)
+                plt.savefig('plots/[scatter_' + cols[i_col] + '_vs_' + y_cat_col + ']' + fname + '.png')
+                plt.close(f4)
+        #plt.show()
 
 
 if __name__=='__main__':
